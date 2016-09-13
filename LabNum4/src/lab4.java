3e373a3a913835d014093a0f470cb59d1b8768a9
Armani Raleigh <armanijolee@gmail.com>
2016-09-13 16:59:08 -0400

    lab4

---
+++ LabNum4/src/lab4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class lab4 {
+
+	public static void main(String[] args) {
+		String choice = "y";
+		while (choice.equals("y")) {
+			
+		
+		System.out.println("Welcome!");
+		System.out.println();
+		System.out.println("Enter an integer");
+		System.out.println("the factorial of ");
+		
+	}
+	
+	System.out.println("Continue? (y/n) ");
+	
+
+	}
+}
