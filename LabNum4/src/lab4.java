7d4a6c7ea79f08bbc1fb138be0b4e281f728eb8f
Armani Raleigh <armanijolee@gmail.com>
2016-09-13 17:58:31 -0400

    Lab 4 complete

--- LabNum4/src/lab4.java
+++ LabNum4/src/lab4.java
@@ -2,19 +2,33 @@ import java.util.Scanner;
 public class lab4 {
 
 	public static void main(String[] args) {
-		String choice = "y";
-		while (choice.equals("y")) {
-			
 		
 		System.out.println("Welcome!");
 		System.out.println();
-		System.out.println("Enter an integer");
-		System.out.println("the factorial of ");
+	
+		
+		String choice = "y";
+		while (choice.equals("y")) {
+			System.out.println("Enter an integer");
+			Scanner userInput = new Scanner(System.in);
+			long input= userInput.nextLong();
+		
+		long fact = 1;
+		
+		for (int i = 1; i <= input; i++) {
+			fact *= i; 
+
+
 		
 	}
-	
-	System.out.println("Continue? (y/n) ");
+		System.out.println("the factorial of " + input + " " + "is" + " "+ fact);
+		
+		System.out.println("Continue? (y/n) ");
+		choice = userInput.nextLine();
+		choice = userInput.nextLine();
+		System.out.println();	
 	
 
 	}
+}
 }
